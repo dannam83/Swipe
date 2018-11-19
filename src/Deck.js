5a4676b218f2dbe84fe0aa0df08e87350fe3655f
Daniel Nam <dannam83@gmail.com>
2018-11-19 08:56:40 -0500

    add a movement functions

--- src/Deck.js
+++ src/Deck.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import { View, Animated, PanResponder } from 'react-native';
 
 class Deck extends Component {
+  constructor(props) {
+    super(props);
+
+    const panResponder = PanResponder.create({
+      // boolean to determine if PanResponder is active on a screen press
+      onStartShouldSetPanResponder: () => true,
+
+      // the function invoked on screen press movements
+      // always takes an event, is an object with info on what was pressed
+      // second arg is gesture, has info on what info is doing with finger on screen
+      onPanResponderMove: (event, gesture) => {
+      },
+
+      // the function on the screen press release
+      onPanResponderRelease: () => {}
+    });
+
+    this.state = { panResponder };
+  }
+
   renderCards() {
     return this.props.data.map(item => {
       return this.props.renderCard(item);
@@ -10,7 +30,7 @@ class Deck extends Component {
 
   render() {
     return (
-      <View>
+      <View {...this.state.panResponder.panHandlers}>
         {this.renderCards()}
       </View>
     );

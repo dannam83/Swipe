fe4d8d5fdafaae166b204b04e851064ee3bc8927
Daniel Nam <dannam83@gmail.com>
2018-11-20 08:37:06 -0500

    stack cards

--- src/Deck.js
+++ src/Deck.js
@@ -91,15 +91,19 @@ class Deck extends Component {
         return (
           <Animated.View
             key={item.id}
-            style={this.getCardStyle()}
+            style={[this.getCardStyle(), styles.cardStyle]}
             {...this.state.panResponder.panHandlers}
           >
             {this.props.renderCard(item)}
           </Animated.View>
         );
       }
-      return this.props.renderCard(item);
-    });
+      return (
+        <View key={item.id} style={styles.cardStyle}>
+          {this.props.renderCard(item)}
+        </View>
+      );
+    }).reverse();
   }
 
   render() {
@@ -110,5 +114,12 @@ class Deck extends Component {
     );
   }
 }
+
+const styles = {
+  cardStyle: {
+    position: 'absolute',
+    width: SCREEN_WIDTH
+  }
+};
 
 export default Deck;
